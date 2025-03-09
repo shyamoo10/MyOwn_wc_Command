3d89980642a329409036d6a825931422673949a0
shyam <rajshyam151@gmail.com>
2025-03-09 15:36:52 +0530

    lines count  functionality is added

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import sys
 
 file= open("test.txt","rb")
 content=  file.read()
+file_text=open("test.txt","r",encoding="utf-8")
+content_txt=  file_text.read()
+
+
 
 
 if sys.argv[1]=="ccwc":
     if sys.argv[2]=="-c":
         print(len(content))
+    elif sys.argv[2]=="-l":
+        line_count= content_txt.split("\n")
+        print(len(line_count))    
     else:
         print("syntax error,use correct flags")  
 else:
